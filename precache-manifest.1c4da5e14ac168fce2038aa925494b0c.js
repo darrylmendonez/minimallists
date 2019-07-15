99dddbf50de3ac360c049d9bf7b87d7817ff3d48
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-07-15 11:19:39 -0400

    Updates

---
+++ precache-manifest.1c4da5e14ac168fce2038aa925494b0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f351caeef61155078245a87296defa4",
+    "url": "/minimallists/index.html"
+  },
+  {
+    "revision": "f46250a1b52b8b0fbdda",
+    "url": "/minimallists/static/css/main.096dc9d3.chunk.css"
+  },
+  {
+    "revision": "1669e293c959c07ab2b7",
+    "url": "/minimallists/static/js/2.0bce724f.chunk.js"
+  },
+  {
+    "revision": "f46250a1b52b8b0fbdda",
+    "url": "/minimallists/static/js/main.77c4d5a8.chunk.js"
+  },
+  {
+    "revision": "18fe2baa3002f72dfb8c",
+    "url": "/minimallists/static/js/runtime~main.6eecd8a6.js"
+  }
+]);
